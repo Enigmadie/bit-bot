d17857071f024603df4bc8556e680fc9e6cb588c
Enigmadie <gagen666@mail.ru>
2025-09-17 02:18:54 +0300

    add account-info method

--- src/api/bybit/mod.rs
+++ src/api/bybit/mod.rs
@@ -1,7 +1,12 @@
 use std::collections::HashMap;
 
+use chrono::Utc;
 use hmac::{Hmac, Mac};
+use reqwest::header::{HeaderMap, HeaderValue};
+use serde::{Deserialize, Serialize};
 use sha2::Sha256;
+
+use crate::utils::config::Env;
 
 #[derive(Debug, Clone)]
 pub struct BybitClient {
@@ -11,7 +16,28 @@ pub struct BybitClient {
 
 type HmacSha256 = Hmac<Sha256>;
 
+#[derive(Serialize, Deserialize)]
+pub struct Res {
+    marginMode: String,
+    updatedTime: String,
+    unifiedMarginStatus: u32,
+    dcpStatus: String,
+    timeWindow: u32,
+    smpGroup: Vec<u32>,
+    isMasterTrader: bool,
+    spotHedgingStatus: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct AccountInfo {
+    pub retCode: i32,
+    pub retMsg: String,
+    pub result: Res,
+}
+
 impl BybitClient {
+    pub const RECV_WINDOW: u16 = 20000;
+
     pub fn new(api_key: String, api_secret: String) -> Self {
         BybitClient {
             api_key,
@@ -19,27 +45,56 @@ impl BybitClient {
         }
     }
 
-    pub fn get_account_info(&self) -> String {
-        // Implementation for fetching account info from Bybit
-        let recv_window = 20000;
+    pub async fn get_account_info(&self) -> Result<AccountInfo, reqwest::Error> {
         let params = HashMap::new();
-        self.create_signature(4214124124, recv_window, params)
+        let timestamp = Utc::now().timestamp();
+        let signature = self.create_signature(timestamp, params);
+
+        let path = "/v5/account/info";
+        let api_url = Env::from_env().api_bybit_url;
+        let url = format!("{}{}", api_url, path);
+
+        let mut headers = HeaderMap::new();
+        headers.insert("X-BAPI-SIGN-TYPE", HeaderValue::from_str("2").unwrap());
+        headers.insert(
+            "X-BAPI-API-KEY",
+            HeaderValue::from_str(&self.api_key).unwrap(),
+        );
+        headers.insert(
+            "X-BAPI-RECV-WINDOW",
+            HeaderValue::from_str(&Self::RECV_WINDOW.to_string()).unwrap(),
+        );
+        headers.insert("X-BAPI-SIGN", HeaderValue::from_str(&signature).unwrap());
+        headers.insert(
+            "X-BAPI-TIMESTAMP",
+            HeaderValue::from_str(&signature).unwrap(),
+        );
+
+        let client = reqwest::Client::new();
+
+        let response = client
+            .get(&url)
+            .headers(headers)
+            .send()
+            .await?
+            .json::<AccountInfo>()
+            .await?;
+
+        Ok(response)
     }
 
     pub fn place_order(&self) {
         // Implementation for placing an order on Bybit
     }
 
-    fn create_signature(
-        &self,
-        timestamp: u64,
-        recv_window: u64,
-        params: HashMap<String, String>,
-    ) -> String {
+    fn create_signature(&self, timestamp: i64, params: HashMap<String, String>) -> String {
         let query_string = serde_urlencoded::to_string(&params).unwrap_or_default();
         let meta = format!(
             "{}{}{}{}",
-            timestamp, &self.api_key, recv_window, query_string
+            timestamp,
+            &self.api_key,
+            Self::RECV_WINDOW,
+            query_string
         );
 
         let mut mac = HmacSha256::new_from_slice(&self.api_key.clone().into_bytes())
--- src/utils/config.rs
+++ src/utils/config.rs
@@ -1,18 +1,19 @@
 use std::env;
 
 #[derive(Debug)]
-pub struct ApiKeys {
+pub struct Env {
     pub api_key_bybit: String,
     pub secret_bybit: String,
     pub api_bybit_url: String,
 }
 
-impl ApiKeys {
-    pub fn from_env() -> Result<Self, env::VarError> {
-        Ok(Self {
-            api_key_bybit: env::var("API_KEY_BYBIT")?,
-            secret_bybit: env::var("SECRET_BYBIT")?,
-            api_bybit_url: env::var("SECRET_BYBIT")?,
-        })
+impl Env {
+    pub fn from_env() -> Self {
+        Self {
+            api_key_bybit: env::var("API_KEY_BYBIT").expect("Missing API_KEY_BYBIT"),
+            secret_bybit: env::var("SECRET_BYBIT").expect("Missing SECRET_BYBIT"),
+            api_bybit_url: env::var("API_BYBIT_URL")
+                .unwrap_or_else(|_| "https://api.bybit.com".to_string()),
+        }
     }
 }
